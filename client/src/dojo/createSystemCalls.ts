6630be54d74ee5e6e690b1486628d49c1069ed3f
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-07-08 06:52:08 +0200

    readme

--- client/src/dojo/createSystemCalls.ts
+++ client/src/dojo/createSystemCalls.ts
@@ -1,60 +1,9 @@
-import { AccountInterface, shortString } from 'starknet';
-import { Entity, Has, HasValue, World, defineSystem, getComponentValue } from '@dojoengine/recs';
-import { uuid } from '@latticexyz/utils';
+import { World } from '@dojoengine/recs';
 import { ClientComponents } from './createClientComponents';
-import { getEntityIdFromKeys } from '@dojoengine/utils';
-import { ContractComponents } from './generated/contractComponents';
 import type { IWorld } from './generated/generated';
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
-export function createSystemCalls(
-	{ client }: { client: IWorld },
-
-	{ Tile, Player }: ClientComponents,
-	world: World
-) {
-	const paint = async ({ account, x, y, color }: { account: AccountInterface; x: number; y: number; color: string }) => {
-		const tileId = uuid();
-
-		console.log('Painting tile', { x, y, color, account });
-
-		Tile.addOverride(tileId, {
-			entity: getEntityIdFromKeys([BigInt(x), BigInt(y)]) as Entity,
-			value: {
-				x,
-				y,
-				color: BigInt(shortString.encodeShortString(color)),
-				owner: BigInt(account.address),
-			},
-		});
-
-		console.log(getComponentValue(Tile, getEntityIdFromKeys([BigInt(x), BigInt(y)]) as Entity));
-
-		try {
-			await client.actions.paint({
-				account,
-				x: x.toString(),
-				y: y.toString(),
-				color: shortString.encodeShortString(color),
-			});
-
-			// Wait for the indexer to update the entity
-			// By doing this we keep the optimistic UI in sync with the actual state
-			// await new Promise<void>((resolve) => {
-			// 	defineSystem(world, [Has(Player)], () => {
-			// 		console.log('Tile updated');
-			// 		resolve();
-			// 	});
-			// });
-		} catch (e) {
-			console.log(e);
-			// Tile.removeOverride(tileId);
-		} finally {
-			// Tile.removeOverride(tileId);
-		}
-	};
-	return {
-		paint,
-	};
+export function createSystemCalls({ client }: { client: IWorld }, { Tile, Player }: ClientComponents, world: World) {
+	return {};
 }
--- readme.md
+++ readme.md
@@ -2,15 +2,43 @@
 
 This is a three.js dojo app enabling users to paint on an infinite grid.
 
-As the world pans, the worlds syncs and subscribes to that part of the world.
+Features:
+
+- Infinite onchain grid
+- Chunk loading of scene as user scrolls
+- Chunk syncing of the State as the user scrolls
+
+### Running
+
+Make sure to install dojo by following this [guide](https://book.dojoengine.org/getting-started)
+
+_Install_
+
+```bash
+curl -L https://install.dojoengine.org | bash
+```
+
+_Run dojo up_
+
+```bash
+dojoup
+```
+
+### Running Locally
+
+_Run in a terminal_
 
 ```
 sh scripts/contracts.sh
 ```
 
+_Run in a terminal_
+
 ```
 sh scripts/indexer.sh
 ```
+
+_Run in a terminal_
 
 ```
 sh scripts/client.sh
